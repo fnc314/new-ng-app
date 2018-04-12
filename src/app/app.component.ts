b0cac6d13d921ad7b224a6c187e03a91bcc6c1aa
Franco N. Colaizzi <fnc314@gmail.com>
2018-04-12 00:02:40 -0400

    new packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'What I want it to be';
+  example = () => 'BLAH';
 }
